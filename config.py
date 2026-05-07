8c89e5875f7d4b756e278c179bb9d9b88b4395fa
zaharfox <voroninzaharfox10@gmail.com>
2026-05-08 00:18:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7981471983:AAHYm03vmirzqpQfMq7fQDZVHbpy1fVnfKc")
 DATABASE_URL = os.getenv("DATABASE_URL", "orchids.db")
